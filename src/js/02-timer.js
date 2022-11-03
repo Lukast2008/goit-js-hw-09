f3a5364786c12bf8e7c721dc72638a6905e67636
Lukast2008 <lukast2008@gmail.com>
2022-11-03 21:22:51 +0200

    HW 9 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,8 @@ import flatpickr from 'flatpickr';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
+import Notiflix from 'notiflix';
+
 flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
@@ -9,13 +11,15 @@ flatpickr('#datetime-picker', {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (Date.now() > new Date(selectedDates[0])) {
-      window.alert('Please choose a date in the future');
+      // window.alert('Please choose a date in the future');
+      Notiflix.Report.info('Please choose a date in the future');
     }
   },
 });
 
 const handelInputTimeValue = document.querySelector('#datetime-picker');
 const handelButtonControlTimer = document.querySelector('[data-start]');
+const timerEl = document.querySelector('.timer');
 
 handelButtonControlTimer.setAttribute('disabled', true);
 
@@ -32,55 +36,59 @@ handelInputTimeValue.addEventListener('change', () => {
   }
 });
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 const timer = {
   intervalId: null,
 
   start(rootSelector, deadline) {
     this.intervalId = setInterval(() => {
       const now = new Date();
-      const diff = searchDateInInput - now;
+      const diff = deadline - now;
 
-      const { days, hours, minutes, seconds } = this.getTimeComponents(diff);
+      if (diff <= 0) {
+        this.stop();
+        return;
+      }
+
+      const { days, hours, minutes, seconds } = this.convertMs(diff);
 
       rootSelector.querySelector('[data-days]').textContent = this.pad(days);
-      rootSelector.querySelector('[data-hours] ').textContent = this.pad(hours);
+      rootSelector.querySelector('[data-hours]').textContent = this.pad(hours);
       rootSelector.querySelector('[data-minutes]').textContent =
         this.pad(minutes);
       rootSelector.querySelector('[ data-seconds]').textContent =
         this.pad(seconds);
-
-      rootSelector.querySelector('[data-days]').dataset.title =
-        this.declensionNum(days, ['день', 'дня', 'днів']);
-      rootSelector.querySelector('[data-hours] ').dataset.title =
-        this.declensionNum(hours, ['година', 'години', 'годин']);
-      rootSelector.querySelector('[data-minutes]').dataset.title =
-        this.declensionNum(minutes, ['хвилина', 'хвилини', 'хвилин']);
-      rootSelector.querySelector('[ data-seconds]').dataset.title =
-        this.declensionNum(seconds, ['секунда', 'секунди', 'секунд']);
     }, 1000);
   },
 
-  getTimeComponents(diff) {
-    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const minutes = Math.floor(diff / 1000 / 60) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
-
-    return {
-      days,
-      hours,
-      minutes,
-      seconds,
-    };
+  stop() {
+    clearInterval(this.intervalId);
   },
 
-  declensionNum(num, words) {
-    return words[
-      num % 100 > 4 && num % 100 < 20
-        ? 2
-        : [2, 0, 1, 1, 1, 2][num % 10 < 5 ? num % 10 : 5]
-    ];
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  },
+
+  pad(value) {
+    return `${value}`.padStart(2, 0);
   },
 };
+
+handelButtonControlTimer.addEventListener('click', () => {
+  timer.start(timerEl, searchDateInInput);
+  handelButtonControlTimer.setAttribute('disabled', true);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const handleButtonSub = document.querySelector('form');
+let delayValue = 0;
+let stepValue = 0;
+let amountValue = 0;
+
+function createPromise({ position, stepValue }) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, stepValue });
+      } else {
+        reject({ position, stepValue });
+      }
+    }, delayValue);
+  });
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  delayValue = Number(delay.value);
+  stepValue = Number(step.value);
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    createPromise(i, stepValue)
+      .then(({ position, delay }) => {
+        Notiflix.Report.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Report.info(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    stepValue += Number(step.value);
   }
 }
+
+handleButtonSub.addEventListener('submit', onSubmit);
