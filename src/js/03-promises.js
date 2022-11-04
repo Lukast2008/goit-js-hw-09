71782776354ea3f8bbb3503f06fea2e47e50b3e8
Lukast2008 <lukast2008@gmail.com>
2022-11-04 20:23:28 +0200

    3 hw done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,14 +5,14 @@ let delayValue = 0;
 let stepValue = 0;
 let amountValue = 0;
 
-function createPromise({ position, stepValue }) {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, stepValue });
+        resolve({ position, delay });
       } else {
-        reject({ position, stepValue });
+        reject({ position, delay });
       }
     }, delayValue);
   });
@@ -21,18 +21,19 @@ function createPromise({ position, stepValue }) {
 function onSubmit(event) {
   event.preventDefault();
   const { delay, step, amount } = event.target.elements;
-  delayValue = Number(delay.value);
-  stepValue = Number(step.value);
+  delayValue = Number(delay.value); // затримка
+  stepValue = Number(step.value); // крок
+  amountValue = Number(amount.value); //цикл
 
-  for (let i = 1; i <= Number(amount.value); i += 1) {
-    createPromise(i, stepValue)
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue)
       .then(({ position, delay }) => {
-        Notiflix.Report.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notiflix.Report.info(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    stepValue += Number(step.value);
+    delayValue += stepValue;
   }
 }
 
